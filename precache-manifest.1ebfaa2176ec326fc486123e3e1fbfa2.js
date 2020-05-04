5ad75078194e0654797f18c529a9e047b88161c8
Deepak raj <deepakraj5497@gmail.com>
2020-05-04 13:44:06 +0530

    Updates

---
+++ precache-manifest.1ebfaa2176ec326fc486123e3e1fbfa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49ec9029f2f87df23efb1db98a91165",
+    "url": "/redux-react/index.html"
+  },
+  {
+    "revision": "63cd0cacc12c7e1ce83d",
+    "url": "/redux-react/static/css/main.ba465bd3.chunk.css"
+  },
+  {
+    "revision": "6093c3b38981a0a5f465",
+    "url": "/redux-react/static/js/2.6c3fd937.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-react/static/js/2.6c3fd937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63cd0cacc12c7e1ce83d",
+    "url": "/redux-react/static/js/main.5a466366.chunk.js"
+  },
+  {
+    "revision": "a4b207005741adc40813",
+    "url": "/redux-react/static/js/runtime-main.e5511a97.js"
+  }
+]);
